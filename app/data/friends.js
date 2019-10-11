4c880614ea53c9609dbf4427289f6b2a8b3f7e4f
Nicholas Ready <Ready.nicholas@gmail.com>
2019-10-10 20:51:40 -0500

    All routing and pages set up. Submit button now sends new friend JSON to the api/friends path

---
+++ app/data/friends.js
@@ -0,0 +1,69 @@
+var friendsArray =
+    [
+        {
+            "name": "Edward",
+            "photo": "https://deepalijunjappa.files.wordpress.com/2015/12/fotografiahomensmanhandsomemen-b7dc170e96def56dd1b55bb438d236fb_h.jpg",
+            "scores": [
+                5,
+                1,
+                4,
+                4,
+                5,
+                1,
+                2,
+                5,
+                4,
+                1
+            ]
+        },
+        {
+            "name": "Dave",
+            "photo": "https://c1.staticflickr.com/1/257/31414963364_08c19c5868_o.jpg",
+            "scores": [
+                3,
+                2,
+                2,
+                4,
+                1,
+                1,
+                5,
+                1,
+                3,
+                1
+            ]
+        },
+        {
+            "name": "Luke",
+            "photo": "https://images.huffingtonpost.com/2016-04-10-1460276866-2126352-unnamed1-thumb.jpg",
+            "scores": [
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1
+            ]
+        },
+        {
+            "name": "Henry",
+            "photo": "https://i.pinimg.com/originals/85/22/e8/8522e8ac32daeef0d58204c689c566e0.jpg",
+            "scores": [
+                3,
+                1,
+                5,
+                1,
+                1,
+                5,
+                1,
+                3,
+                2,
+                1
+            ]
+        }
+    ];
+
+module.exports = friendsArray;
